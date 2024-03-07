77488018849bc0e4162af26642ac2c89516b0849
RE70-DECEMBER <158739296+RE70-DECEMBER@users.noreply.github.com>
2024-03-07 12:18:39 -0600

    Add files via upload

---
+++ Zanden.py
@@ -0,0 +1,24 @@
+#Made By RE70-DECEMBER
+import os 
+
+os.system("clear")
+os.system("cowsay Welcome Back Bitch")
+print()
+print("1. RoJaker")
+print("2. Freckle Bot ")
+print("3. Check For Updates ")
+print()
+menu = input("Menu --> ")
+
+if menu == "1": 
+  os.system("cd")
+  os.system("cd RoJaker")
+  os.system("python3 rojaker.py")
+elif menu == "2":
+  print("work in progress")
+  input("click enter to go back")
+elif menu == "3":
+  print("work in progress")
+  input("click enter to go back")
+else: 
+  print("thats a invaild option buddy")
